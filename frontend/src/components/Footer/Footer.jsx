ae31f2075ce820bbcf5345033c00b3f319db66da
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-08-11 23:06:20 +0530

    Make footer responsive

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ import { FaGithub } from "react-icons/fa";
 
 function Footer() {
   return (
-    <div className="h-32 text-home-black flex flex-col justify-center items-center p-8 z-20 sticky bottom-0 bg-white">
-      <div className="flex w-1/2 pt-5 pb-2 border-t-2 text-3xl justify-center items-center gap-20">
+    <div className="h-32 text-home-black flex flex-col justify-center items-center p-4 sm:p-8 z-20 sticky bottom-0 bg-white">
+      <div className="flex w-full sm:w-1/2 pt-5 pb-2 border-t-2 text-3xl justify-center items-center gap-10 sm:gap-20">
         <a
           href="https://github.com/UjjawalGusain/" //change it later on
           target="_blank"
