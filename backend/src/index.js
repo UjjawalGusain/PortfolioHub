c53b408f7d95a8d59066fff6218c3078c5f824cd
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-08-13 10:39:42 +0530

    Fixing vercel bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,25 @@
-import connect_database from "./db/index.js"
-import dotenv from "dotenv"
-import { app } from "./app.js"
+import connect_database from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
-    path: "./.env"
-})
+  path: "./.env",
+});
 
+// connect_database()
+// .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//         console.log(`Server running at port: ${process.env.PORT}`);
+//     })
+// })
+// .catch((error) => {
+//     console.log(`MongoDB connection failed: ${error}`);
+// })
 
 connect_database()
-.then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`Server running at port: ${process.env.PORT}`);
-    })
-})
-.catch((error) => {
-    console.log(`MongoDB connection failed: ${error}`);
-}) 
+  .then(() => {
+    console.log(`MongoDB connected successfully`);
+  })
+  .catch((error) => {
+    console.error(`MongoDB connection failed: ${error}`);
+  });
