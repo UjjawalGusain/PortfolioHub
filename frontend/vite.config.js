88c6d7b606c42289a6d45966ce9086ba2c51c76f
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-08-13 21:15:21 +0530

    Bug fix bracket

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,5 @@ import http from "https";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'https://portfolio-hub-backend.vercel.app',
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ''),
-  //       agent: new http.Agent()
-  //     }
-    
-  //   }
-  },
   plugins: [react()],
 })
