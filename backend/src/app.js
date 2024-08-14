7157a4da3f59f3ffa7f30093fbebdde72a491cd5
UjjawalGusain <ujjawalgusain31@gmail.com>
2024-08-14 06:32:12 +0530

    Allowing localhost origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,10 +4,22 @@ import cookieParser from "cookie-parser"
 
 
 const app = express()
+const allowedOrigins = [
+    'https://portfolio-hub-omega.vercel.app',
+    'http://localhost:5173'
+];
+
 app.use(cors({
-    origin: 'https://portfolio-hub-omega.vercel.app',
+    origin: function(origin, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
-}))
+}));
+
 
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
